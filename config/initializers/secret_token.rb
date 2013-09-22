539fa3e46015f4b09868e159c9ab14d4089bf0f9
tomeresk <tomeresk@gmail.com>
2013-09-22 04:05:27 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbFriends::Application.config.secret_token = '21c11a6cd3fd76b3e9fdbec796f374d47898b0ae2dc16f68994a960096657ab82514d719da0ca2c4d25bfe43e6177a6e48142ffbc0d057fd5542f62d2859a1a5'
